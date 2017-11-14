dec0d556d7e8c7950b38c26e226ec3ebcbef1172
lucassiqueira08 <lalvessiqueira8@gmail.com>
2017-11-14 20:51:08 -0200

    Correcao das ref

--- Projeto/core/admin.py
+++ Projeto/core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 
-from core.modelos import Aluno, Disciplina, Curso
+from core.models import Aluno, Disciplina, Curso
 
 # Register your models here.
 admin.site.register(Curso)
 admin.site.register(Disciplina)
 admin.site.register(Aluno)
+
--- Projeto/core/views.py
+++ Projeto/core/views.py
@@ -18,3 +18,21 @@ def contato(request):
         print(request.POST['mensagem'])
     return render(request,"contato.html")
 '''
+def index(request):
+	return render(request, 'index.html')
+	
+def login(request):
+	return render(request, 'login.html')
+	
+def esquecisenha(request):
+	return render(request, 'esquecisenha.html')
+	
+def contato(request):
+	return render(request, 'contato.html')
+	
+def cadastro(request):
+	return render(request, 'cadastro.html')
+	
+def aluno(request):
+	return render(request, "aluno.html")
+	
