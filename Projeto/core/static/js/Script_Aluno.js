64a5e6ca52c105827aa9fa96261c0201a844bfa9
lucas11200 <lucas11200@hotmail.com>
2017-11-19 02:20:04 -0200

    Alterações na pagina Aluno

---
+++ Projeto/core/static/js/Script_Aluno.js
@@ -0,0 +1,11 @@
+function Show_hide(){
+	var pixels=0;
+	alert("teste")
+	if (document.getElementById('#btnMenuLateral').checked==true){
+		
+		document.getElementById('menu_left').css('marginLeft',pixels);	
+	}
+	
+
+
+}
